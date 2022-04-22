964328ef3c33a39ecf6def65eee4bae882ba1d8b
angel <dehainaut.angelique@orange.fr>
2022-04-22 12:00:47 +0200

    workbench routing config project manager

--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
 <?php
+
+use App\Router;
+require __DIR__ . '/../includes.php';
+
+session_start();
+
+try {
+    Router::route();
+}
+
+catch (ReflectionException $e) {
+    echo "La connexion a échoué";
+}
