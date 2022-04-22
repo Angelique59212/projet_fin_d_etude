0f14a9fec2cdbb13e5d91a993502b9c02657471c
angel <dehainaut.angelique@orange.fr>
2022-04-22 18:14:48 +0200

    include entity

--- include.php
+++ include.php
@@ -4,5 +4,9 @@ require __DIR__ .'/Config.php';
 require __DIR__ .'Model/Connect.php';
 
 require __DIR__ .'Model/Entity/AbstractEntity.php';
+require __DIR__ .'Model/Entity/Article.php';
+require __DIR__ .'Model/Entity/Comment.php';
+require __DIR__ .'Model/Entity/Role.php';
+require __DIR__ .'Model/Entity/User.php';
 
 require __DIR__ .'Controller/AbstractController.php';
