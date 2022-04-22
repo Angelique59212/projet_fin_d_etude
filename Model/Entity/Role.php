926920973d83ece62d626b68f106b033a725f87f
angel <dehainaut.angelique@orange.fr>
2022-04-22 18:13:15 +0200

    entity

---
+++ Model/Entity/Role.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Model\Entity;
+
+use AbstractEntity;
+
+class Role extends AbstractEntity
+{
+    private ?string $roleName;
+
+    /**
+     * @return string|null
+     */
+    public function getRoleName(): ?string
+    {
+        return $this->roleName;
+    }
+
+    /**
+     * @param string|null $roleName
+     * @return Role
+     */
+    public function setRoleName(?string $roleName): self
+    {
+        $this->roleName = $roleName;
+        return $this;
+    }
+
+
+}
--- include.php
+++ include.php
@@ -1,1 +1,8 @@
 <?php
+
+require __DIR__ .'/Config.php';
+require __DIR__ .'Model/Connect.php';
+
+require __DIR__ .'Model/Entity/AbstractEntity.php';
+
+require __DIR__ .'Controller/AbstractController.php';
