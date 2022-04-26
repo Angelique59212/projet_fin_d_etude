cbd5aa8d19e8275c5d8632c6a7a42eb58f847a78
angel <dehainaut.angelique@orange.fr>
2022-04-26 15:26:50 +0200

    dyslexie

--- Controller/ArticleController.php
+++ Controller/ArticleController.php
@@ -8,6 +8,6 @@ class ArticleController extends AbstractController
 {
     public function index()
     {
-        $this->render('home/home');
+        $this->render('article/dyslexie');
     }
 }
---
+++ View/article/dyslexie.html.php
@@ -0,0 +1,41 @@
+<div class="container">
+    <div class="container-definition">
+        <h2 class="definition">Définition de la Dyslexie (<span class="website">Site Amélie</span>)</h2>
+        <p>
+            Les troubles du langage écrit (dyslexie, dysorthographie et dysgraphie) se manifestent par des confusions et
+            inversions de sons et de lettres, des fautes d’orthographe, voire une écriture lente et illisible.
+        </p>
+        <p>
+            Ils sont repérables très tôt par l’entourage familial et scolaire. Toutefois, on en précise les
+            caractéristiques à la fin du CE1 seulement.
+        </p>
+    </div>
+
+
+    <div class="container-second">
+        <div class="symptom">
+            <h3 class="definition">Symptômes</h3>
+            <p>
+                Les enfants rencontrent diverses difficultés, qu'il est possible de s'apercevoir avant même l'entrée en primaire.
+                Ces enfants ont un retard de langage, des difficultés à construire des phrases, des difficultés à écrire
+                leur prénom.
+                Mais comme évoqué dans la définition, cette pathologie est évoqué en primaire.
+                L'enfant sera en difficulté face à la lecture ce qui entraîne une mauvaise compréhension, il inversera sans
+                doutes des lettres, en oubliera ou en remplacera. Mais aussi des difficultés à l'écriture, à la différenciation
+                de certaines lettres de l'alphabet.
+                Il peut également y avoir des troubles de la coordination et de la motricité.
+            </p>
+            <p>
+                La dyslexie n'est pas un trouble lié à l'intelligence mais à un trouble neurobiologique.
+            </p>
+        </div>
+        <div class="image-symptom">
+            <img src="/assets/img/dyslexie/symptom.jpg" alt="165 symptomes dyslexie">
+        </div>
+    </div>
+
+
+
+
+
+</div>
