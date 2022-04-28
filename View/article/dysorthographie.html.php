82d96878feca510f2fc7616f76b7f1d6c865ce75
angel <dehainaut.angelique@orange.fr>
2022-04-28 16:10:59 +0200

    symptom / definition dysorthographie

--- View/article/dysorthographie.html.php
+++ View/article/dysorthographie.html.php
@@ -1,1 +1,38 @@
-<?php
+<div class="container">
+    <div class="container-definition">
+        <h2 class="definition">Définition de la Dysorthograpgie (<span class="website">Site dys-positif</span>)</h2>
+        <p>
+            La dysorthographie est un trouble de la famille des DYS, qui se manifeste par un défaut
+            majeur dans l’acquisition et l’assimilation de l’orthographe. L’enfant qui en souffre éprouve de grandes
+            difficultés à maitriser les règles orthographiques, ce qui entraîne une altération de l’écriture, aussi bien
+            spontanée que dictée.
+        </p>
+    </div>
+
+    <div class="container-second">
+        <div class="symptom">
+            <h3 class="definition">Symptômes</h3>
+            <p>
+                Il a des difficultés à écrire des mots inconnus ou qu'il ne rencontre pas souvent. Il rencontre des
+                difficultés en grammaire. Il aura du mal à faire la différence d'écriture de mots qui se ressemblent mais
+                ne s'écrivent pas pareil.
+                On rencontrera par conséquent des fautes d'orthographe, une absence de conjugaison, ainsi que des phrases
+                peu cohérente.
+                Il aura des difficultés d'orientation spatio-temporelle, mais également des problèmes de mémorisation.
+                Son vocabulaire pauvre entraînera un retard de langage.
+            </p>
+        </div>
+        <div class="image-symptom">
+            <img src="/assets/img/dysorthographie/dysorthographie%20ecole.jpg" alt="dysorthopgraphie ecole">
+        </div>
+    </div>
+
+    <div class="container-second">
+        <div class="symptom">
+            <h3 class="definition">A l'école</h3>
+            <p>
+
+            </p>
+        </div>
+    </div>
+</div>
